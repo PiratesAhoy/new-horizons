32e11ac4efe2b12e456cf4eb3de6bc6767af8423
Hammie <sam@hammie.be>
2022-07-27 21:48:17 +0200

    Add "StormX" to build version number

--- PROGRAM/INTERFACE/mainmenu.c
+++ PROGRAM/INTERFACE/mainmenu.c
@@ -23,7 +23,7 @@ void InitInterface(string iniName)
 
 	SendMessage(&GameInterface,"lsls",MSG_INTERFACE_MSG_TO_NODE,"BACKPICTURE",3, "interfaces\mainback");
 
-	string currentversion = " - Build " + BUILDVERSION;
+	string currentversion = " - Build " + BUILDVERSION + " StormX";
 	CreateString(true,"Version","Pirates Ahoy! Community" + currentversion,FONT_QUESTBOOK,COLOR_GREEN_LIGHT,-20,430,SCRIPT_ALIGN_LEFT,0.8);
 	CreateString(true,"Info","http://"+PA_INET,FONT_QUESTBOOK,COLOR_BLUE_LIGHT,-20,470,SCRIPT_ALIGN_LEFT,0.8);
 
