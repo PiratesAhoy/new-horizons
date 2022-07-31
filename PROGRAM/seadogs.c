b6e629086c1ff4f64e9ba463aaaa945a25936b33
Hammie <sam@hammie.be>
2022-07-31 13:19:54 +0200

    Add missing native functions

--- PROGRAM/seadogs.c
+++ PROGRAM/seadogs.c
@@ -94,6 +94,10 @@ native int RDTSC_E(int iRDTSC);
 //int onesec; // NK for speedtests.
 
 native int SetTexturePath(int iLevel, string sPath);
+native int SetGlowParams(float fBlurBrushSize, int Intensivity, int BlurPasses);
+native int RPrint(int x, int y, string sPrint);
+native int GetTexture(string fileName);
+native void ReleaseTexture(int texId);
 
 #libriary "script_libriary_test"
 #libriary "dx9render_script_libriary"
