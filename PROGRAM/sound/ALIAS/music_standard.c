3718b1b99df8c0b62870dc1c709ed67412a0cc7c
Hammie <sam@hammie.be>
2023-01-26 21:50:28 +0100

    Use PiratesAhoy theme for main title music

--- PROGRAM/sound/ALIAS/music_standard.c
+++ PROGRAM/sound/ALIAS/music_standard.c
@@ -7,7 +7,7 @@ void InitMusic()
 
 	//============ MAIN MENU ========================
 	// added by NK so it can be done in code.
-	tmpref.music_main_menu.f1.name = "MUSIC\Main_Title.ogg";						// PB: Cutthroat Island
+	tmpref.music_main_menu.f1.name = "MUSIC\piratesahoy_theme.ogg";
 
 	//============ MAIN THEME ========================
 	tmpref.music_main_theme.f1.name = "MUSIC\Main_Theme.ogg";
