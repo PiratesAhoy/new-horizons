92bc353f7d8388e4c8ac375718ac8cdcd372e057
Hammie <sam@hammie.be>
2023-04-21 10:29:06 +0200

    Update patch creation script

--- create-patch.py
+++ create-patch.py
@@ -23,12 +23,12 @@ def add_directory(z, path, target = None):
     for base, dirs, files in os.walk(path):
         for file in files:
             fn = os.path.join(base, file)
-            print(fn)
+            # print(fn)
             z.write(fn, fn.replace(path, target or ""))
 
 with ZipFile('patch.zip', 'w', compression=ZIP_LZMA) as z:
     for file in files:
-        if file != "" and not file.startswith("installer/") and not file.endswith(".py"):
+        if file != "" and file != "engine.ini" and not file.startswith("installer/") and not file.endswith(".py"):
             z.write(file)
     z.write("installer/resources/engine.ini", 'engine.ini')
     z.write("E:/Projects/PiratesAhoy/storm-engine/cmake-build-release/bin/crashpad_handler.exe", 'crashpad_handler.exe')
