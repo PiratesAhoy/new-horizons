29875795a9bb01907619c7c43f57569c1fa897a8
Hammie <sam@hammie.be>
2024-11-03 13:27:16 +0100

    Use engine version pa15.0.0-beta4

--- installer/publish.py
+++ installer/publish.py
@@ -44,7 +44,7 @@ with open("userversion.txt", 'w') as f:
         tomlkit.dump(mod, mod_file)
 
 # Download engine
-dload.save_unzip("https://github.com/PiratesAhoy/storm-engine/releases/download/pa15.0.0-beta.3/storm-engine.release-steam-false.zip", "engine")
+dload.save_unzip("https://github.com/PiratesAhoy/storm-engine/releases/download/pa15.0.0-beta.4/storm-engine.release-steam-false.zip", "engine")
 
 def add_file(source_file, target_file=None):
     if target_file is None:
